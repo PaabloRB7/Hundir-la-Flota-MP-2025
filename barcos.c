e01ea5958dc72c76ac00fb066a2d31c7adf8d493
PabloSalas11 <pablosalasibanez@gmail.com>
2025-03-19 17:30:55 +0100

    primeros cambios en barco

---
+++ barcos.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void portaaviones(){
+    int vida=6;
+}
+
+void acorazado(){
+    int vida=5;
+}
+
+void crucero(){
+    int vida=4;
+}
+
+void destructor(){
+    int vida=3;
+}
+
+void fragata(){
+    int vida=2;
+}
+
+void submarino(){
+    int vida=1;
+}
