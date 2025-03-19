84c09c116fe663836d9cdd99fd11088cd9e087a5
Ángel C. <164225127+cornejopi@users.noreply.github.com>
2025-03-19 17:50:21 +0100

    Letra y num ---> fila y columna
    Eliminada la inclusion de letras en vez de numeros para las filas

--- disparos.c
+++ disparos.c
@@ -4,8 +4,7 @@
 //Prototipos
 int disparo_manual(int tablero[][N], int tablero_v[][N]); //Letra y num se corresponden a la dirección del disparo y tablero al tablero del jugador al que dispara
 int disparo_automatico(int tablero[][N], int tablero_v[][N]);
-int disparo(int letra, int num, char tablero[][N],char tablero_v[][N]);
-int letra_num(char letra);
+int disparo(int fila, int columna, char tablero[][N],char tablero_v[][N]);
 
 //Main
 int main(){
@@ -18,39 +17,38 @@ int main(){
 
 
 //Funciones
-int disparo(int letra, int num, char tablero[][N],char tablero_v[][N]){
+int disparo(int fila, int columna, char tablero[][N],char tablero_v[][N]){
 int tocado = 0, i, j, hundido = 1;
 char barco;
-if(tablero[letra][num] != '*' && tablero[letra][num] != 'T' && tablero[letra][num] != 'H'){
+if(tablero[fila][columna] != '*' && tablero[fila][columna] != 'T' && tablero[fila][columna] != 'H'){
     tocado = 1;
-    barco = tablero[letra][num];
-    tablero[letra][num] = 'T';
+    barco = tablero[fila][columna];
+    tablero[fila][columna] = 'T';
     for(i = 0;i <= N || hundido = 0;i++)          //Se comprueba si el barco está hundido tras el impacto
        for(j = 0;j <= N || hundido = 0;j++)
           if(barco == tablero[i][j])
             hundido = 0;
     if(hundido == 1){
         tocado = 2;
-        tablero[letra][num] = 'H';
-        tablero_v[letra][num] = 'H';
+        tablero[fila][columna] = 'H';
+        tablero_v[fila][columna] = 'H';
         //FALTA: Bucle que convierta los tocados adyacentes en hundidos
     }
     else
-        tablero_v[letra][num] = 'T';
+        tablero_v[fila][columna] = 'T';
 }
 else
-    tablero_v[letra][numero] = '*';
+    tablero_v[fila][columna] = '*';
 
 return tocado;
 }
 
 int disparo_manual(int tablero[][N], int tablero_v[][N]){
-int numletra, num, resultado;
-char letra;
+int fila, column, resultado;
 
-printf("\nIntroduce las coordenadas del disparo (separando la letra y el número por un espacio): ");
-scanf("%c %d", &letra, &num);
-resultado = disparo(letra_num(letra), num, tablero, tablero_v);
+printf("\nIntroduce las coordenadas del disparo (separando la fila y columna por un espacio): ");
+scanf("%d %d", &fila, &column);
+resultado = disparo(fila, columna, tablero, tablero_v);
 
 switch(resultado)
  case 1:
@@ -64,18 +62,6 @@ switch(resultado)
 
 
 return resultado;
-}
-
-int letra_num(char letra){
-int num, i;
-char letrasm[9] = "ABCDEFGHI";
-char letras[9] = "abcdefghi";
-
-for(i = 0;i <= 9;i++)
-    if(letrasm[i] == letra || letras[i] == letra)
-       num = i;
-
-return num;
 }
 
 int disparo_automatico(){
